da1b35f35cd405c0cd182ec53a70ed47fdddc73b
Alisson Wilker <alissonwilker@gmail.com>
2017-08-03 22:29:21 -0300

    Adicionando PrimeFaces ao projeto.

--- src/main/java/br/edu/ifb/view/mbean/OlaMundoBean.java
+++ src/main/java/br/edu/ifb/view/mbean/OlaMundoBean.java
@@ -1,12 +1,15 @@
 package br.edu.ifb.view.mbean;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 
 @ManagedBean
-public class OlaMundoBean {
+@RequestScoped
+public class OlaMundoBean implements Serializable {
 	public String getHorario() {
 		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
 		return "Atualizado em " + sdf.format(new Date());
